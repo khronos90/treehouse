4ea0084acd04f202d8147e03d5cc17c0d6d0c9ab
Bruno Fleischer <bruno.fleischer@toyota.com>
2023-10-16 15:07:02 -0300

    Refactored what is your name into a fn

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use std::io::stdin;
 
-fn main() {
-    println!("Hello, what's your name?");
+fn what_is_your_name() -> String {
     let mut your_name = String::new();
 
-    stdin().read_line(&mut your_name).unwrap();
+    stdin()
+        .read_line(&mut your_name)
+        .expect("Expected a string");
+    your_name
+}
 
-    println!("What up my dude {}", your_name);
+fn main() {
+    println!("Hello, what's your name?");
+    println!("What up my dude {}", what_is_your_name());
 }
