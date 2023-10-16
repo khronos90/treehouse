fbf9bc684c61ad4b36234415d74faf2843dbfb43
Bruno Fleischer <bruno.fleischer@toyota.com>
2023-10-16 15:04:29 -0300

    Added a greeter program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, what's your name?");
+    let mut your_name = String::new();
+
+    stdin().read_line(&mut your_name).unwrap();
+
+    println!("What up my dude {}", your_name);
 }
